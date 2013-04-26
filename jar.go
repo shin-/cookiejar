c0ea942d3d6f933a2d8e33cad9eac75283241afc
shin- <joffrey@dotcloud.com>
2013-04-26 08:20:37 -0700

    Removed typedef

--- jar.go
+++ jar.go
@@ -6,27 +6,25 @@ import(
     "sync"
 )
 
-type CookieSlice []*http.Cookie
-
 type CookieJar struct {
-    data map[string]CookieSlice
+    data map[string][]*http.Cookie
     lock sync.Mutex
 }
 
-func (jar *CookieJar) SetCookies(u *url.URL, cookies CookieSlice) {
+func (jar *CookieJar) SetCookies(u *url.URL, cookies []*http.Cookie) {
     jar.lock.Lock()
     jar.data[u.Host] = cookies
     jar.lock.Unlock()
 }
 
-func (jar *CookieJar) Cookies(u *url.URL) CookieSlice {
+func (jar *CookieJar) Cookies(u *url.URL) []*http.Cookie {
     // FIXME: This is a very naive implementation
     return jar.data[u.Host]
 }
 
 func NewCookieJar() CookieJar {
     return CookieJar{
-        data: make(map[string]CookieSlice),
+        data: make(map[string][]*http.Cookie),
         lock: sync.Mutex{},
     }
 }
